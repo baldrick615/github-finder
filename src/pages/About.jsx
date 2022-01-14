b4ec92ed4b2b4c47b9d886331af77164b26a082d
Tim McGrath <tmcgrath28@gmail.com>
2022-01-14 13:15:42 -0600

    building pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search Github profiles and see profile details. This
+        project is part of a "Modern React Front to Back" course on Udemy.
+      </p>
+      <p className='text-lg text-grey-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </div>
+  );
+}
+
+export default About;
